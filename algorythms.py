a663e263f2be8d126e38c715635b9777cfd32205
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-04-30 21:33:40 -0400

    Hex to Decimal & Cat years, Dog years added

--- algorythms.py
+++ algorythms.py
@@ -477,5 +477,31 @@
 
 # print(sum_mul(2, 2))
 
+
 # ###################################################
-# # 31
+# # 31 Hex to Decimal
+
+# def hex_to_dec(s):
+#     return int(s, 16)
+
+# print('34')
+
+# ###################################################
+# # 32  Cat years, Dog years
+
+
+# def human_years_cat_years_dog_years(human_years):
+#     if human_years == 1:
+#         cat_years = dog_years = 15
+#     elif human_years == 2:
+#         cat_years = dog_years = 24  # 15 + 9
+#     else:
+#         cat_years = 24 + (human_years - 2) * 4
+#         dog_years = 24 + (human_years - 2) * 5
+#     return [human_years, cat_years, dog_years]
+
+# # Example test
+# print(human_years_cat_years_dog_years(10))  # [10, 24 + 8*4 = 56, 24 + 8*5 = 64]
+
+
+
