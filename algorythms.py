d6b446b7bdecd3bc44567e53ae81c65fc9755f1a
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-05-02 23:58:14 -0400

    sum positive

--- algorythms.py
+++ algorythms.py
@@ -505,4 +505,8 @@
 
 # ###################################################
 # # 33 Sum of positive
+def positive_sum(arr):
+    return sum([num for num in arr if num>=0])
+
+print(positive_sum([1,-2,3,4,5]))
 
